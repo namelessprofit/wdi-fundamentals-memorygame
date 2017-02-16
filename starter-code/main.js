ef4c61b3dca3b16b3cc236fba16073fe5fe416f7
Archy Posada <archyposada@Archys-MacBook-Pro.local>
2017-02-15 17:39:25 -0800

    adding dynamic html via js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,23 @@
 console.log("JS file is connected to HTML! Woo!")
-cardOne="king";
-cardTwo="king";
-cardThree="queen";
-cardFour="queen";
+
+cardOne ="king";
+cardTwo ="king";
+cardThree ="queen";
+cardFour ="queen";
+
+/*if (cardTwo===cardFour) {alert('Sorry try again.')
+}
+else if (cardOne===cardThree) {alert('Sorry try again.')
+}
+else if (cardTwo===cardOne) {alert('You found match!')
+}
+else if (cardThree===cardFour) {alert('You found a match!')
+}*/
+var board = document.getElementById('board');
+
+for (var i = 0; i < 4; i++) {
+  var div = document.createElement('DIV');
+  div.className += 'card';
+  board.appendChild(div);
+  console.log(board);
+}
