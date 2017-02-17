bbb7b9bee3393b86c0b81c0b06bcd77942f0da99
namelessprofit <posadaarchy@gmail.com>
2017-02-16 17:08:33 -0800

    final solution

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,23 +1,51 @@
 console.log("JS file is connected to HTML! Woo!")
 
-cardOne ="king";
-cardTwo ="king";
-cardThree ="queen";
-cardFour ="queen";
+// var cards = ['queen','queen','king','king'];
+// var cardsInPlay = [];
+//   for (var i = 0; i < cards.length; i++) {
+// var board = document.getElementById('board');
+// var cardElement = document.createElement('div');
+// cardElement.classname = 'card';
+// }
 
-/*if (cardTwo===cardFour) {alert('Sorry try again.')
-}
-else if (cardOne===cardThree) {alert('Sorry try again.')
-}
-else if (cardTwo===cardOne) {alert('You found match!')
-}
-else if (cardThree===cardFour) {alert('You found a match!')
-}*/
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
 var board = document.getElementById('board');
 
-for (var i = 0; i < 4; i++) {
-  var div = document.createElement('DIV');
-  div.className += 'card';
-  board.appendChild(div);
-  console.log(board);
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
+    var gameCard = document.createElement('div');
+    gameCard.className = 'card';
+    gameCard.setAttribute('data-card', cards[i]);
+    gameCard.addEventListener('click', isTwoCards);
+    board.appendChild(gameCard);
+    board.appendChild(gameCard);
+  }
 }
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>";
+	} else {
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>";
+	}
+
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
+
+createBoard();
